3151eb05e2da5c1ddf01dc59c58dba720430cc13
brad-basilio <brad.basilioppy@gmail.com>
2025-11-23 16:25:39 -0500

    impleemntacion de right bar

--- resources/js/components/Adminto/RightBar.jsx
+++ resources/js/components/Adminto/RightBar.jsx
@@ -56,7 +56,7 @@ const RigthBar = () => {
         <a href="#" className="right-bar-toggle float-end">
           <i className="mdi mdi-close"></i>
         </a>
-        <h4 className="font-16 m-0 text-white">Configurar tema</h4>
+        <h4 className="font-16 m-0 text-white">Theme Settings</h4>
       </div>
       <div className="tab-content pt-0">
 
@@ -64,41 +64,41 @@ const RigthBar = () => {
 
           <div className="p-3">
             <div className="alert alert-warning" role="alert">
-              <strong>Modifica </strong> el tema, el menu, la barra superior, etc.
+              <strong>Customize </strong> the theme, menu, top bar, etc.
             </div>
 
-            <h6 className="fw-medium font-14 mt-4 mb-2 pb-1">Tema principal</h6>
+            <h6 className="fw-medium font-14 mt-4 mb-2 pb-1">Main Theme</h6>
             <div className="form-check form-switch mb-1">
               <input type="checkbox" className="form-check-input" name="color-scheme-mode"
                 id="light-mode-check" defaultChecked={settings.theme == 'dark'} />
-              <label className="form-check-label" htmlFor="light-mode-check">{settings.theme == 'dark' ? 'Modo oscuro' : 'Modo claro'}</label>
+              <label className="form-check-label" htmlFor="light-mode-check">{settings.theme == 'dark' ? 'Dark Mode' : 'Light Mode'}</label>
             </div>
-            <h6 className="fw-medium font-14 mt-4 mb-2 pb-1">Ancho de la ventana</h6>
+            <h6 className="fw-medium font-14 mt-4 mb-2 pb-1">Window Width</h6>
             <div className="form-check form-switch mb-1">
               <input type="checkbox" className="form-check-input" name="width" id="fluid-check"
                 defaultChecked={settings.width == 'boxed'} />
-              <label className="form-check-label" htmlFor="fluid-check">{settings.width == 'boxed' ? 'Encuadrado' : 'Completo'}</label>
+              <label className="form-check-label" htmlFor="fluid-check">{settings.width == 'boxed' ? 'Boxed' : 'Fluid'}</label>
             </div>
-            <h6 className="fw-medium font-14 mt-4 mb-2 pb-1">Posicion del menu y el nabvar</h6>
+            <h6 className="fw-medium font-14 mt-4 mb-2 pb-1">Menu and Navbar Position</h6>
 
             <div className="form-check form-switch mb-1">
               <input type="checkbox" className="form-check-input" name="menus-position"
                 id="fixed-check" defaultChecked={settings.menuPosition == 'scrollable'} />
-              <label className="form-check-label" htmlFor="fixed-check">{settings.menuPosition == 'scrollable' ? 'Posision dinamica' : 'Posicion fija'}</label>
+              <label className="form-check-label" htmlFor="fixed-check">{settings.menuPosition == 'scrollable' ? 'Scrollable' : 'Fixed'}</label>
             </div>
 
-            <h6 className="fw-medium font-14 mt-4 mb-2 pb-1">Color del menu</h6>
+            <h6 className="fw-medium font-14 mt-4 mb-2 pb-1">Menu Color</h6>
 
             <div className="form-check form-switch mb-1">
               <input type="radio" className="form-check-input" name="leftsidebar-color" value="light"
                 id="light-check" defaultChecked={!settings.menuColor || settings.menuColor == 'light'} />
-              <label className="form-check-label" htmlFor="light-check">Claro</label>
+              <label className="form-check-label" htmlFor="light-check">Light</label>
             </div>
 
             <div className="form-check form-switch mb-1">
               <input type="radio" className="form-check-input" name="leftsidebar-color" value="dark"
                 id="dark-check" defaultChecked={settings.menuColor == 'dark'} />
-              <label className="form-check-label" htmlFor="dark-check">Oscuro</label>
+              <label className="form-check-label" htmlFor="dark-check">Dark</label>
             </div>
 
             <div className="form-check form-switch mb-1">
@@ -110,44 +110,44 @@ const RigthBar = () => {
             <div className="form-check form-switch mb-3">
               <input type="radio" className="form-check-input" name="leftsidebar-color" value="gradient"
                 id="gradient-check" defaultChecked={settings.menuColor == 'gradient'} />
-              <label className="form-check-label" htmlFor="gradient-check">Gradiente</label>
+              <label className="form-check-label" htmlFor="gradient-check">Gradient</label>
             </div>
-            <h6 className="fw-medium font-14 mt-4 mb-2 pb-1">Medida del menu</h6>
+            <h6 className="fw-medium font-14 mt-4 mb-2 pb-1">Menu Size</h6>
 
             <div className="form-check form-switch mb-1">
               <input type="radio" className="form-check-input" name="leftsidebar-size" value="default"
                 id="default-size-check" defaultChecked={!settings.menuSize || settings.menuSize == 'default'} />
-              <label className="form-check-label" htmlFor="default-size-check">Por defecto</label>
+              <label className="form-check-label" htmlFor="default-size-check">Default</label>
             </div>
 
             <div className="form-check form-switch mb-1">
               <input type="radio" className="form-check-input" name="leftsidebar-size" value="condensed"
                 id="condensed-check" defaultChecked={settings.menuSize == 'condensed'} />
-              <label className="form-check-label" htmlFor="condensed-check">Condensado <small>(Tamaño extra pequeño)</small></label>
+              <label className="form-check-label" htmlFor="condensed-check">Condensed <small>(Extra small size)</small></label>
             </div>
 
             <div className="form-check form-switch mb-1">
               <input type="radio" className="form-check-input" name="leftsidebar-size" value="compact"
                 id="compact-check" defaultChecked={settings.menuSize == 'compact'} />
-              <label className="form-check-label" htmlFor="compact-check">Compacto <small>(Tamaño pequeño)</small></label>
+              <label className="form-check-label" htmlFor="compact-check">Compact <small>(Small size)</small></label>
             </div>
-            <h6 className="fw-medium font-14 mt-4 mb-2 pb-1">Informacion del Usuario (Menu)</h6>
+            <h6 className="fw-medium font-14 mt-4 mb-2 pb-1">User Information (Menu)</h6>
 
             <div className="form-check form-switch mb-1">
               <input type="checkbox" className="form-check-input" name="leftsidebar-user" value="fixed"
                 id="sidebaruser-check" defaultChecked={settings.userInfo != false} />
-              <label className="form-check-label" htmlFor="sidebaruser-check">{settings.userInfo == 'false' ? 'Ocultar' : 'Mostrar'}</label>
+              <label className="form-check-label" htmlFor="sidebaruser-check">{settings.userInfo == 'false' ? 'Hide' : 'Show'}</label>
             </div>
-            <h6 className="fw-medium font-14 mt-4 mb-2 pb-1">Color de la barra superior</h6>
+            <h6 className="fw-medium font-14 mt-4 mb-2 pb-1">Top Bar Color</h6>
 
             <div className="form-check form-switch mb-1">
               <input type="checkbox" className="form-check-input" name="topbar-color" value="dark"
                 id="darktopbar-check" defaultChecked={settings.navbarColor == 'dark'} />
-              <label className="form-check-label" htmlFor="darktopbar-check">{settings.navbarColor == 'dark' ? 'Oscuro' : 'Acorde al cuerpo'}</label>
+              <label className="form-check-label" htmlFor="darktopbar-check">{settings.navbarColor == 'dark' ? 'Dark' : 'Match Body'}</label>
             </div>
 
             <div className="d-grid mt-4">
-              <button className="btn btn-primary" id="resetBtn">Restablecer</button>
+              <button className="btn btn-primary" id="resetBtn">Reset</button>
               {/* <a href="https://1.envato.market/admintoadmin" className="btn btn-danger mt-3"
                 target="_blank"><i className="mdi mdi-basket me-1"></i> Purchase Now</a> */}
             </div>
